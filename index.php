4079c0543d7a134dea22bb8afc3eec635ff55a35
jaksonqwik <ndryg547@gmail.com>
2023-04-09 17:00:05 +0300

    start

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style.css">
+    <title>Лента</title>
+</head>
+<body>
+    <div>
+        <h1>Страйт</h1>
+        <ul class='qus'>
+            <li><a href="/link/question.php">Задайте вопрос</li>
+        </ul>
+    </div>
+    <hr>
+</body>
+</html>
+
+
+
+<?php
+
+
+?>
