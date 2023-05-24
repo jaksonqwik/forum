0f2eececfe7a71e6b0e799258bd090454cdfb9c2
jaksonqwik <ndryg547@gmail.com>
2023-05-24 12:27:25 +0300

    style_no_pass_log

--- login/login.php
+++ login/login.php
@@ -40,14 +40,13 @@ $db->connect();
             </div>
         </form>
     </div>
-    <br>
     <div class="no_pass_login">
-        <?php
+        <p style="text-align:center"><?php
         if(isset($_SESSION['msg'])){
             echo $_SESSION['msg'];
             unset($_SESSION['msg']);
         } 
-        ?>
+        ?></p>
     </div>
     <br>
     <div class="link">
